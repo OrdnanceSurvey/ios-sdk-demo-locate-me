733c4edc31e1669be36182bf29a77999bc569c54
rob-murray <robmurray17@gmail.com>
2012-10-03 09:40:24 +0100

    Added StaticFrameworks dir

---
+++ OSLocateMe/OSLocateMe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OSLocateMe
+//
+//  Created by rmurray on 02/10/2012.
+//  Copyright (c) 2012 OrdnanceSurvey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
